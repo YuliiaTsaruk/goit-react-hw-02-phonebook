4e7b4dc1db205a7b49852d16467daec7ebd2b63d
YuliiaTsaruk <julia.tsaruck@gmail.com>
2023-11-12 16:59:33 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { Filter } from './Filter/Filter';
+// import { ContactList } from './ContactList/ContactList';
+// import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContact = newContact => {
+    const contact = {
+      ...newContact,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <div>
+          <label>
+            Name
+            <input type="text" name="name" required />
+          </label>
+          <button>Add contact</button>
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <ul></ul>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,4 @@
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
+
+// export const ContactForm = ({ onAdd }) => { }
